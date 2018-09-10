edccb557b24afb77ddb005589148db32cb4e6d8b
chaizy <1617402283@qq.com>
2018-09-10 17:46:27 +0800

    青马创翼基础服务

---
+++ qm-config-client/src/main/java/com/qmcy/ConfigClient.java
@@ -0,0 +1,13 @@
+package com.qmcy;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+
+@SpringBootApplication
+public class ConfigClient {
+
+	public static void main(String[] args) {
+		new SpringApplicationBuilder(ConfigClient.class).web(true).run(args);
+	}
+
+}
